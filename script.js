ebd93945059cba6250e4d5b3f4cca8eaa01da5b4
sethclaw <sethclaw10@gmail.com>
2017-03-02 16:48:49 -0700

    finish

--- script.js
+++ script.js
@@ -22,29 +22,37 @@ scotchApp.config(function($routeProvider) {
         // route for the contact page
         .when('/example', {
             templateUrl : 'pages/example.html',
-            controller  : 'contactController'
+            controller  : 'exampleController'
         });
 });
 
 // create the controller and inject Angular's $scope
 scotchApp.controller('mainController', function($scope) {
     // create a message to display in our view
-    $scope.message = 'Everyone come and see how good I look!';
+    $scope.message = 'Welcome to our single page application! Feel free to have a look around.' + 
+    				'This is the home page but we also have a short about section and a short Angular.js example.' +
+    				'Notice how the whole page does not while clicking between tabs.';
 });
-
-
 scotchApp.controller('aboutController', function($scope) {
-    $scope.message = 'Look! I am an about page.';
+    $scope.message = 'As you can see, This content on the website changes when you click the links'+
+    					'but the page does not reload. This is because we are using the AngularJS Route module.' +
+    					'This type of page is called a Single Page Application. Check out our AngularJS Example to-do list!';
 });
 
-scotchApp.controller('contactController', function($scope) {
-    $scope.task = []
+scotchApp.controller('exampleController', function($scope) {
+    $scope.task = [{id: 0, toDo:'Burn Homework', done:false}];
     $scope.addTask = function(){
       var newId = $scope.task.length;
       newId++;
       $scope.task.push(
-        {id: newId, toDo: $scope.addToDo}
+        {id: newId, toDo: $scope.addToDo, done:false}
       )
-            $scope.textInp.placeholder = '';
     }
+    $scope.remove = function() {
+      var oldList = $scope.task;
+      $scope.task = [];
+      angular.forEach(oldList, function(x) {
+          if (!x.done) $scope.task.push(x);
+      });
+    };
 });
